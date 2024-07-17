b063c2f13d928c8712ddc56362139cd0506fdb83
parry_peng <parry_peng0809@qq.com>
2024-07-17 20:01:35 +0800

    V0.0.6 tmp_2

--- app/src/main/java/com/example/livestockweightandtempims/ui/enter/EnterFragment.java
+++ app/src/main/java/com/example/livestockweightandtempims/ui/enter/EnterFragment.java
@@ -24,8 +24,8 @@ public class EnterFragment extends Fragment {
         binding = FragmentEnterBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textEnter;
-        enterViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textEnter;
+//        enterViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/livestockweightandtempims/ui/query/QueryFragment.java
+++ app/src/main/java/com/example/livestockweightandtempims/ui/query/QueryFragment.java
@@ -24,8 +24,8 @@ public class QueryFragment extends Fragment {
         binding = FragmentQueryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textQuery;
-        queryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textQuery;
+//        queryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
