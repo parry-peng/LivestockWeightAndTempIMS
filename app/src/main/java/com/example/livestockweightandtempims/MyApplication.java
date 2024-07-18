6a223a9d715b8c5370e1eed66e7d7427cb09e140
parry_peng <parry_peng0809@qq.com>
2024-07-18 21:47:42 +0800

    V0.0.8 tmp_4

---
+++ app/src/main/java/com/example/livestockweightandtempims/MyApplication.java
@@ -0,0 +1,30 @@
+package com.example.livestockweightandtempims;
+
+import android.app.Application;
+import androidx.room.Room;
+
+import com.example.livestockweightandtempims.database.LivestockDatabase;
+
+public class MyApplication extends Application {
+    private static MyApplication mApp;
+    private LivestockDatabase livestockDatabase;
+    public static MyApplication getInstance() {
+        return mApp;
+    }
+    public LivestockDatabase getBookDB() {
+        return livestockDatabase;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mApp = this;
+
+        livestockDatabase = Room.databaseBuilder(this, LivestockDatabase.class, "livestock")
+                // 允许迁移数据库（发生数据库变更时，Room默认删除原数据库再创建新数据库。如此一来原来的记录会丢失，故而要改为迁移方式以便保存原有记录）
+                .addMigrations()
+                // 允许在主线程中操作数据库（Room默认不能在主线程中操作数据库）
+                .allowMainThreadQueries()
+                .build();
+    }
+}
